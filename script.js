8b2e2dce8f95c4337a38995c866e79381ceea1ae
Anastasiia Shlapak <anastasiashlapak@icloud.com>
2022-11-18 08:06:39 +0100

    Change script

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-function returnFamilyProperty(){
-    
-    const family = {
-        Arthur: 'father',
-        Mandy: 'mother',
-        Kevin: 'son'
+function returnFamilyProperty() {
+  const family = {};
+  for (let i = 0; i < 3; i++) {
+    let key = prompt("Enter name");
+    let member = prompt("Enter member type");
+    if (
+      isNaN(key) === true &&
+      key !== null &&
+      key !== "" &&
+      isNaN(member) === true &&
+      member !== null &&
+      member !== ""
+    ) {
+      family[key] = member;
     }
-    return family
+  }
+  return family;
 }
-const content = returnFamilyProperty()
+const content = returnFamilyProperty();
 
-for (const key in content ) {
-console.log(`${key} is a ${content[key]}`)
+for (const key in content) {
+  console.log(`${key} is a ${content[key]}`);
 }
